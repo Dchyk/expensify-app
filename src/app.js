98c4c93fb2004988616b7d642e8ac57ae7421486
Alex Dzwonchyk <alex.dzwonchyk@gmail.com>
2018-01-21 12:50:30 -0500

    Removed dummy expense data

--- src/app.js
+++ src/app.js
@@ -13,14 +13,6 @@ import { setTimeout } from 'timers';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 120 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 320, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1095 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
